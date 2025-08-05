4737cc39a7a0c62bfd0416c7be1bbf3f4b866fd2
androd865-dotcom <and.rod865@gmail.com>
2025-08-05 15:51:38 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,14 +1,1 @@
-#include "../s21_string.h"
 
-int main() {
-  char str1[1024] = "";
-  char str2[1024] = "";
-  char *ch = NULL;
-  const char *format = "|%s|\n";
-  int res1 = s21_sprintf(str1, format, ch);
-  int res2 = sprintf(str2, format, ch);
-
-  printf("%d: %s%d: %s", res1, str1, res2, str2);
-
-  return 0;
-}
